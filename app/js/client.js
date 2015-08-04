bbeff303d0a0c14887ceb21ac6f5cb3a1ccc494a
Dewey Luu <deweyluu@gmail.com>
2015-08-04 12:14:17 -0700

    added buttons

--- app/js/client.js
+++ app/js/client.js
@@ -9,4 +9,10 @@ var carController = cars.controller('carController',
 		$scope.make = 'acura';
 		$scope.model = 'integra';
 		$scope.year = 1999;
+		$scope.carLoader = function() {
+			$scope.car = $scope.year + " " + $scope.make + " " + $scope.model;
+		}
+		$scope.anotherCar = function() {
+			$scope.car = "2006 Toyota 4Runner";
+		}
 }]);
